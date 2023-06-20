175889857fdd848d0b4340af98c75ab06382fb5d
Hiroaki Ozasa <37623652+OzasaHiro@users.noreply.github.com>
2023-06-20 15:50:18 +0900

    Update main.dart
    use Local_model

--- main.dart
+++ main.dart
@@ -1,8 +1,30 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+import 'dart:io' as io;
+import 'package:flutter/services.dart';
+
 //import 'package:google_ml_kit/google_ml_kit.dart';
+
+
+Future<String> _getModel(String assetPath) async {
+  if (io.Platform.isAndroid) {
+    return 'flutter_assets/$assetPath';
+  }
+  final path = '${(await getApplicationSupportDirectory()).path}/$assetPath';
+  await io.Directory(dirname(path)).create(recursive: true);
+  final file = io.File(path);
+  if (!await file.exists()) {
+    final byteData = await rootBundle.load(assetPath);
+    await file.writeAsBytes(byteData.buffer
+        .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+  }
+  return file.path;
+}
 
 void main() {
   runApp(MyApp());
@@ -30,20 +52,44 @@ class ObjectDetection extends StatefulWidget {
 class _ObjectDetectionState extends State<ObjectDetection> {
   File? _imageFile;
   String? _result;
-  final ImageLabelerOptions options = ImageLabelerOptions(confidenceThreshold: 0.8);
+  String? modelPath;
   ImageLabeler? _imageLabeler;
   final ImagePicker _picker = ImagePicker();
 
   @override
   void initState() {
     super.initState();
-    _imageLabeler = ImageLabeler(options: options);
+    _getModelAndStartImageLabeler();
+  }
+
+  void _getModelAndStartImageLabeler() async {
+    modelPath = await _getModel('assets/model.tflite');
+    if (modelPath != null) {
+      final options = LocalLabelerOptions(modelPath: modelPath!, confidenceThreshold: 0.7);
+      _imageLabeler = ImageLabeler(options: options);
+    }
   }
 
   void _getImageAndDetectObjects(ImageSource source) async {
     final PickedFile? pickedImage = await _picker.getImage(source: source);
     if (pickedImage != null) {
-      final inputImage = InputImage.fromFilePath(pickedImage.path);
+      // 元の画像ファイルを保存
+      File originalImageFile = File(pickedImage.path);
+
+      // 画像をリサイズ
+      final Uint8List? compressedImage = await FlutterImageCompress.compressWithFile(
+        pickedImage.path,
+        minWidth: 400,
+        minHeight: 300,
+      );
+
+      // リサイズした画像を一時的なファイルに保存
+      final tempDir = await getTemporaryDirectory();
+      final path = '${tempDir.path}/temp.jpg';
+      File file = File(path)..writeAsBytesSync(compressedImage!);
+
+      // 一時的なファイルのパスを使用して入力画像を作成
+      final inputImage = InputImage.fromFilePath(file.path);
       final List<ImageLabel> labels = await _imageLabeler?.processImage(inputImage) ?? [];
 
       ImageLabel? highestConfidenceLabel;
@@ -61,8 +107,9 @@ class _ObjectDetectionState extends State<ObjectDetection> {
         result = "Can't detect";
       }
 
+      // 結果の更新
       setState(() {
-        _imageFile = File(pickedImage.path);
+        _imageFile = originalImageFile;  // 元の画像を使用
         _result = result;
       });
     }
